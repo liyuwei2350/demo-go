1868684b28715ad751e5f4379205b32181ac9545
liyuwei <liyuwei2350@outlook.com>
2026-04-14 17:27:08 +0800

    update

--- main.go
+++ main.go
@@ -10,6 +10,10 @@ import (
 func main() {
 	r := gin.Default()
 
+	r.GET("/test", func(c *gin.Context) {
+		c.String(http.StatusOK, "ok")
+	})
+
 	// /auth 路由供 Nginx auth_request 使用
 	r.GET("/auth", func(c *gin.Context) {
 
@@ -22,10 +26,10 @@ func main() {
 		// 但如果我们在响应头中返回内容，Nginx 可以通过 auth_request_set 提取它并传给后端
 		if isGrayUser {
 			// 如果是灰度用户，设置灰度标识 Header
-			c.Header("X-Gray-Env", "canary")
+			c.Header("X-Gray-Env", "true")
 		} else {
 			// 如果不是灰度用户，可以设置成普通标识或不设置
-			c.Header("X-Gray-Env", "prod")
+			c.Header("X-Gray-Env", "false")
 		}
 
 		// 鉴权成功，必须返回 200 或其他 2xx 状态码，Nginx 才会放行请求
